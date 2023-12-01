caab35bb31fd1b5f944f2fd1a1c00aa6e9f6c615
Zoya Nasir <k22031112@kcl.ac.uk>
2023-12-01 18:49:02 +0000

    final commit

--- things/views.py
+++ things/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    html = "<html><head><title>Things</title></head><body><h1>Things</h1></body></html>"
-    return HttpResponse(html)
-
+    return render(request, 'home.html', {'form': form})
