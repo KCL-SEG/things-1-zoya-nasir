d6716382a84e574196ecf3fa79a78d8229d0cc54
Zoya Nasir <k22031112@kcl.ac.uk>
2023-12-04 16:09:34 +0000

    final

--- things/views.py
+++ things/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 
 def home(request):
-    html = "<html><head><title>Things</title></head><body><h1>Things</h1></body></html>"
+    html = "<html><head><title>Things</title></head></html>"
     return HttpResponse(html)
 
